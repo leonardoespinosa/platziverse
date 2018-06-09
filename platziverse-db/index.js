f69ed07921b5ff9a5a5b6e33e630817f8ecfade4
leonardoespinosa <leonardoespinosa@realbind.com>
2018-06-09 18:38:52 -0500

    Add DB setups in index module

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,24 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequalize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  // Define models relationship with sequalize
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  // Verify DB connection
+  await sequalize.authenticate()
+
+  // Create tables in DB if then not exists
+  sequalize.sync()
+
   const Agent = {}
   const Metric = {}
 
