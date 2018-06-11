70fd71406f561731686dd7d2ea8dc47c491cb3b3
leonardoespinosa <leonardoespinosa@realbind.com>
2018-06-10 23:27:11 -0500

    Integrate MQTT Server with DB

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,19 +4,32 @@ const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('platziverse-db')
 
 const backend = {
-    type: 'redis',
-    redis,
-    return_buffers: true
+  type: 'redis',
+  redis,
+  return_buffers: true
 }
 
 const settings = {
-    port: 1883,
-    backend
+  port: 1883,
+  backend
+}
+
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'platzi',
+  password: process.env.DB_PASS || 'platzi',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: s => debug(s),
+  operatorsAliases: false
 }
 
 const server = new mosca.Server(settings)
+
+let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
@@ -27,20 +40,25 @@ server.on('clientDisconnected', client => {
 })
 
 server.on('published', (packet, client) => {
-  debug(`Received: ${packet.topic}`)    
-  debug(`Payload: ${packet.payload}`)    
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
 })
 
-server.on('ready', () => {
-    console.log(`${chalk.green('[platziverse-mqtt]')} server is running`);
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+
+  Agent = services.Agent
+  Metric = services.Metric
+
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
 
 server.on('error', handleFatalError)
 
-function handleFatalError(err){
-    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
-    console.error(err.stack)
-    process.exit(1)
+function handleFatalError (err) {
+  console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
 }
 
 process.on('uncaughtException', handleFatalError)
