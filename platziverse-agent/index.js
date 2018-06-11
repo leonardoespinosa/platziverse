97923e24bb0b2338111e9109055daa191f41b12b
leonardoespinosa <leonardoespinosa@realbind.com>
2018-06-11 11:08:19 -0500

    Add timer in event emmiter

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -3,8 +3,32 @@
 const EventEmmiter = require('events')
 
 class PlatziverseAgent extends EventEmmiter {
-  constructor(){
+  constructor(opts){
     super()
+
+    this._options = opts
+    this._started = false
+    this._timer = null
+  }
+
+  connect() {
+    if(!this._started){
+      this._started = true
+      this.emit('connected')      
+      
+      const opts = this._options
+      this._timer = setInterval(()=> {
+          this.emit('agent/message', 'this is a message')
+      }, opts.interval)
+    }
+  }
+
+  disconnect () {
+    if(this._started){
+        clearInterval(this._timer)
+        this._started = false
+        this.emit('disconnected')
+    }
   }
 }
 
