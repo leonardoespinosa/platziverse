6c536599750eb905aa0dc27d0c9578a9dccd1038
leonardoespinosa <leonardoespinosa@realbind.com>
2018-06-11 22:52:42 -0500

    Add test in api module

--- platziverse-api/tests/api-tests.js
+++ platziverse-api/tests/api-tests.js
@@ -2,7 +2,41 @@
 
 const test = require('ava')
 const request = require('supertest')
-const server = require('../server')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+const agentFixtures = require('./fixtures/agent')
+
+let sandbox = null
+let server = null
+let dbStub = null
+let AgentStub = {}
+let MetricStub = {}
+
+test.beforeEach(async () => {
+  sandbox = sinon.sandbox.create()
+  
+  dbStub = sandbox.stub()
+  dbStub.returns(Promise.resolve({
+      Agent: AgentStub,
+      Metric: MetricStub
+  }))
+
+  AgentStub.findConnected = sandbox.stub()
+  AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected))
+
+  const api = proxyquire('../api',{
+    'platziverse-db': dbStub
+  })
+
+  server = proxyquire('../server',{
+    './api': api
+  })
+})
+
+test.afterEach(() => {
+  sandbox && sandbox.restore()
+})
 
 test.serial.cb('/api/agents', t => {
   request(server)
@@ -11,8 +45,9 @@ test.serial.cb('/api/agents', t => {
     .expect('Content-Type', /json/)
     .end((err, res) => {
       t.falsy(err, 'should not return an error')
-      let body = res.body
-      t.deepEqual(body, {}, 'response body should be the expected')
+      let body = JSON.stringify(res.body)
+      let expected = JSON.stringify(agentFixtures.connected)
+      t.deepEqual(body, expected, 'response body should be the expected')
       t.end()
     })
 })
