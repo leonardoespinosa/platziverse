3e9a9533af911c8cf86d3cf8ff03f7e26add428b
leonardoespinosa <leonardoespinosa@realbind.com>
2018-06-11 19:33:54 -0500

    Add api module

---
+++ platziverse-api/server.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const http = require('http')
+const express = require('express')
+const chalk = require('chalk')
+
+const api = require('./api')
+
+const port = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+
+app.use('/api', api)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
+})
