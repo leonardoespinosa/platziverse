c060407b9f7c038c477219a53d5001cbbce5b0ea
leonardoespinosa <leonardoespinosa@realbind.com>
2018-06-13 20:30:22 -0500

    Proxy creation in web module

---
+++ platziverse-web/config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+  apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTI4OTM5NDM0fQ.ijzBAvPX7HA32DGl9w37WAxpfumfNq0NUaI8iOnX7Bg'
+}
